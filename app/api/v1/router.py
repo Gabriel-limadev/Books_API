09dbc68a0e84a8dc9c9831471168dda9dd8e70a1
Gabriel-limadev <gabrielvitor180701@gmail.com>
2026-01-14 22:01:55 -0300

    deixando apenas a rota de scraping privada

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -11,12 +11,12 @@ private_router = APIRouter(
 # Rotas públicas
 public_router.include_router(auth.router, prefix='/auth', tags=['Auth'])
 public_router.include_router(health.router, prefix='/health', tags=['Health'])
+public_router.include_router(books.router, prefix='/books', tags=['Books'])
+public_router.include_router(categories.router, prefix='/categories', tags=['Categories'])
+public_router.include_router(stats.router, prefix='/stats', tags=['Stats'])
 
 # Rotas privadas
 private_router.include_router(scraping.router, prefix='/scraping', tags=['Scraping'])
-private_router.include_router(books.router, prefix='/books', tags=['Books'])
-private_router.include_router(categories.router, prefix='/categories', tags=['Categories'])
-private_router.include_router(stats.router, prefix='/stats', tags=['Stats'])
 
 # Router principal
 router = APIRouter()
